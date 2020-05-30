d4c2a90852e1ffed715c48a54163bc994d0aa9f1
Maxim Biro <nurupo.contributions@gmail.com>
2020-05-30 10:14:35 -0400

    Allow specifying prefixes

--- mingw-ldd.py
+++ mingw-ldd.py
@@ -22,7 +22,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from __future__ import print_function
 import pefile
 import os
 import sys
@@ -36,41 +35,29 @@ def get_dependency(filename):
     return deps
 
 
-def dep_tree(root, prefix=None):
-    if not prefix:
-        arch = get_arch(root)
-        #print('Arch =', arch)
-        prefix = '/usr/'+arch+'-w64-mingw32/bin'
-        #print('Using default prefix', prefix)
+def dep_tree(root, prefixes):
     dep_dlls = dict()
 
-    def dep_tree_impl(root, prefix):
+    def dep_tree_impl(root):
         for dll in get_dependency(root):
             if dll in dep_dlls:
                 continue
-            full_path = os.path.join(prefix, dll)
-            if os.path.exists(full_path):
-                dep_dlls[dll] = full_path
-                dep_tree_impl(full_path, prefix=prefix)
-            else:
-                dep_dlls[dll] = 'not found'
+            dep_dlls[dll] = 'not found'
+            for prefix in prefixes:
+                full_path = os.path.join(prefix, dll)
+                if os.path.exists(full_path):
+                    dep_dlls[dll] = full_path
+                    dep_tree_impl(full_path)
 
-    dep_tree_impl(root, prefix)
+    dep_tree_impl(root)
     return (dep_dlls)
 
 
-def get_arch(filename):
-    type2arch= {pefile.OPTIONAL_HEADER_MAGIC_PE: 'i686',
-                pefile.OPTIONAL_HEADER_MAGIC_PE_PLUS: 'x86_64'}
-    pe = pefile.PE(filename)
-    try:
-        return type2arch[pe.PE_TYPE]
-    except KeyError:
-        sys.stderr.write('Error: unknown architecture')
-        sys.exit(1)
-
 if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        sys.exit("Usage: {} PE_FILE DLL_LOOKUP_DIR [DLL_LOOKUP_DIR ...]".format(sys.argv[0]))
     filename = sys.argv[1]
-    for dll, full_path in dep_tree(filename).items():
+    prefixes = [os.path.abspath(p) for p in sys.argv[2:]]
+    for dll, full_path in dep_tree(filename, prefixes).items():
         print(' ' * 7, dll, '=>', full_path)
 
